558cdbbc0f83949aef7bd28fc8b288bbb206f920
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-18 22:41:41 +0300

    ft: updated main file with component links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { UserRepoComponent } from './user-repo/user-repo.component';
+import { ReposComponent } from './repos/repos.component';
+import { UsersComponent } from './users/users.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavigationComponent,
+    HeroSectionComponent,
+    UserRepoComponent,
+    ReposComponent,
+    UsersComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
