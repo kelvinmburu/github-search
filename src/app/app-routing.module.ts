f9ab2aedea41b5d37e4a99835c93ccab6cd1f43c
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-19 23:12:01 +0300

    build: imported routing dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRepoComponent } from './user-repo/user-repo.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+// remember to add navigation path to routes
+
+const routes: Routes = [
+  {path: 'users', component: UsersComponent},
+  {path: 'user-repo', component: UserRepoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [UsersComponent, UserRepoComponent]
