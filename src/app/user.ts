0e92507ec52001223415521ca58ff3fd1e17954c
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-19 23:09:09 +0300

    build: added new constructor for api variables

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
+// export class User {
+//   photo: any;
+//   userName: any;
+//   bio: any;
+//   followers: any;
+//   following: any;
+//   repos: any;
+// }
+
 export class User {
+    constructor(public photo: string, public userName: string, public followers:number, public following:number, public repos:number, public bio: string) { }
 }
