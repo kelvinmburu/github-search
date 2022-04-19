1976b32d7127987e62e8dd60dd5413556ba071e5
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-19 23:11:07 +0300

    build: imported needed dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { UserRepoComponent } from './user-repo/user-repo.component';
 import { ReposComponent } from './repos/repos.component';
 import { UsersComponent } from './users/users.component';
 import { FooterComponent } from './footer/footer.component';
-import { UserService } from './search-service/user.service';
+import { SearchService } from './search-service/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     NavigationComponent,
-    HeroSectionComponent,
     UserRepoComponent,
     ReposComponent,
     UsersComponent,
     FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [UserService],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [SearchService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
