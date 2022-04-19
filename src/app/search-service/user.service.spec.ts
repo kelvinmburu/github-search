442cc2bf7459f3225d3c893c9be3dfc998b9aa2c
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-19 23:08:04 +0300

    refactor: corrected typo in property names

--- src/app/search-service/user.service.spec.ts
+++ src/app/search-service/user.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { SearchService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let service: SearchService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(SearchService);
   });
 
   it('should be created', () => {
