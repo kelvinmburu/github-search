4e3a5053fe2c17da108a467f4bc562b6783af49b
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-19 23:06:52 +0300

    build: imported needed dependecies

--- src/app/user-repo/user-repo.component.ts
+++ src/app/user-repo/user-repo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserRepo } from '../user-repo';
 
 @Component({
   selector: 'app-user-repo',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRepoComponent implements OnInit {
 
-  constructor() { }
+  @Input() repos:UserRepo[] =[]
 
   ngOnInit(): void {
   }
