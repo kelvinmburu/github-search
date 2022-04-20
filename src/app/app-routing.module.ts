8ca833e8e2f9054f8e3f6ea3c54d9ddd6ccc1e27
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-20 08:22:20 +0300

    st: formatted and idented code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,16 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { ReposComponent } from './repos/repos.component';
 import { UsersComponent } from './users/users.component';
 
-
 const routes: Routes = [
-  {path: '', redirectTo: '/navigation', pathMatch: 'full'},
-  {path: 'users', component: UsersComponent},
-  {path: 'repos', component: ReposComponent},
+  { path: '', redirectTo: '/navigation', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'repos', component: ReposComponent },
   //{path: 'navigation', component: NavigationComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
-export const routingComponents = [UsersComponent, ReposComponent]
+export class AppRoutingModule {}
+export const routingComponents = [UsersComponent, ReposComponent];
